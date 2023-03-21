4a74207e074fed1c1f092fba21a7a17f12ca5b54
andreureyfor <116097797+andreureyfor@users.noreply.github.com>
2023-03-21 17:54:05 +0100

    Initial commit

---
+++ src/uf2/practica/futbol/main/UF2AppFutbol.java
@@ -0,0 +1,24 @@
+package uf2.practica.futbol.main;
+
+import static uf2.practica.futbol.primerNivellDescomposicio.PrimerNivellDescomposicio.mostrarMenu;
+
+/**
+ * Aquesta classe conte el programa per interactuar amb la classificacio de la lliga de futbol español
+ * @author andreu
+ */
+public class UF2AppFutbol {
+    
+    /**
+     * Al metode main cridarem el menu per a selecionar quina opcio volem fer a la classificacio
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+        
+        mostrarMenu();
+        
+    }
+    
+    
+    
+}
